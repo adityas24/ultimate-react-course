8dd5220045c2f6aceb57e5b68343e997a8acf1f6
Aditya Singh <adisingh1519@gmail.com>
2023-07-10 11:56:29 +0530

    Building a form and handling sumissions

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,10 +1,15 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div>
       <Logo />
       <Form />
       <PackingList />
-      <Stats/>
+      <Stats />
     </div>
   );
 }
@@ -22,7 +27,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌️</button>
+    </li>
+  );
 }
 
 function Stats() {
