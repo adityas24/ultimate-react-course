9ebebda13a946fac6e58cf92c5b541b8c4bd55cd
Aditya Singh <adisingh1519@gmail.com>
2023-07-10 11:31:16 +0530

    Building the layout

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,3 +1,36 @@
 export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats/>
+    </div>
+  );
+}
 
+function Logo() {
+  return <h1>🌴️ Far away 👜️</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍️ trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        👜️ You have X items on your list, and you are already packed X (X%)
+      </em>
+    </footer>
+  );
 }
