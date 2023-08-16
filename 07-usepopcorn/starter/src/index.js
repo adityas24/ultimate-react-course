adfbc17d4c3925e40c62524bec558358984f9a28
Aditya Singh <adisingh1519@gmail.com>
2023-08-16 14:04:21 +0530

    trash

--- 07-usepopcorn/starter/src/index.js
+++ 07-usepopcorn/starter/src/index.js
@@ -8,7 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      size={24}
+      color="red"
+      className="test"
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color="red" className="test" />
   </React.StrictMode>
 );
-
